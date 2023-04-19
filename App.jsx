0cdc6df7b5ee41669be78410e22737909bbabf4e
José Acuña <79762654+NestcafeN@users.noreply.github.com>
2023-04-19 03:21:21 -0400

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -17,7 +17,7 @@ const GeneradorCodigo = () => {
   const [codigo, setCodigo] = useState('');
 
   const generarCodigo = () => {
-    const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
     let nuevoCodigo = '';
     for (let i = 0; i < 6; i++) {
       nuevoCodigo += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
@@ -39,6 +39,11 @@ const obtenerImagenes = async () => {
 
 const App = () => {
   const [imagenes, setImagenes] = useState([]);
+  const [perrosRechazados, setPerrosRechazados] = useState([]);
+  const [perrosCandidatos, setPerrosCandidatos] = useState([]);
+  const [perrosAceptados, setPerrosAceptados] = useState([]);
+  const [imagenSeleccionada, setImagenSeleccionada] = useState(null);
+  
   
   useEffect(() => {
     const obtenerTodasLasImagenes = async () => {
@@ -56,17 +61,40 @@ const App = () => {
       }
       
       setImagenes(nuevasImagenes);
+      setImagenSeleccionada(nuevasImagenes[0][0]);
     }
     
     obtenerTodasLasImagenes();
   }, []);
   
   return (
-    <Box display="flex" justifyContent="center">
-      <Grid container spacing={2}>
+    <Box display="flex" justifyContent="center" alignItems={"flex-start"}>
+      <Grid container spacing={10}>
         <Grid item xs={12}>
-          <Typography variant="h3" align="center" gutterBottom>Tinder Peludo</Typography>
+          <Typography variant="h1" align="center" gutterBottom>Tinder Peludo</Typography>
+          <Typography variant="h3" align="Left">Rechazados</Typography>
+        <List>
+        {perrosRechazados.map((perro) => (
+          <ListItem key={perro.id}>
+            <Typography>{perro.nombre}</Typography>
+          </ListItem>
+        ))}
+      </List>
+        
+        <Typography variant="h3" align="center"gutterBottom>Candidatos</Typography>
+        
+        <List>
+        {perrosCandidatos.map((perro) => (
+          <ListItem key={perro.id}>
+            <Typography>{perro.nombre}</Typography>
+          </ListItem>
+        ))}
+      </List>
+
+        <Typography variant="h3" align="Right">Aceptados</Typography>
+        
         </Grid>
+
         {imagenes.map((columna, i) => (
           <Grid item xs={12} md={4} key={i}>
             {columna.map((imagen) => (
