7584195bb6cb406a9c2b0d35f225925cde864b28
Ru1do <116591871+Ru1do@users.noreply.github.com>
2023-04-19 15:47:53 -0400

    correcion

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import { Grid, Card, CardMedia, Button } from "@mui/material";
 
-function App() {
-  const [count, setCount] = useState(0)
+const PaginaPerros = () => {
+  const [imagenPerro, setImagenPerro] = useState("");
+  const [perrosAceptados, setPerrosAceptados] = useState([]);
+  const [perrosRechazados, setPerrosRechazados] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((respuesta) => respuesta.json())
+      .then((datos) => setImagenPerro(datos.message));
+  }, []);
+
+  const aceptarPerro = () => {
+    setPerrosAceptados([...perrosAceptados, imagenPerro]);
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((respuesta) => respuesta.json())
+      .then((datos) => setImagenPerro(datos.message));
+  };
+
+  const rechazarPerro = () => {
+    setPerrosRechazados([...perrosRechazados, imagenPerro]);
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((respuesta) => respuesta.json())
+      .then((datos) => setImagenPerro(datos.message));
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <Grid container spacing={2}>
+      <Grid item xs={4}>
+        {/* Columna de perros rechazados */}
+        {perrosRechazados.map((perro) => (
+          <Card key={perro} sx={{ maxWidth: 400, borderRadius: 3 }}>
+            <CardMedia component="img" image={perro} alt="Perro" />
+          </Card>
+        ))}
+      </Grid>
+      <Grid item xs={4}>
+        {/* Columna de perros candidatos */}
+        <Card sx={{ maxWidth: 400, borderRadius: 3 }}>
+          <CardMedia component="img" image={imagenPerro} alt="Perro" />
+        </Card>
+        <Grid container spacing={2} sx={{ mt: 2 }}>
+          <Grid item xs={6}>
+            <Button variant="contained" color="secondary" onClick={rechazarPerro}>
+              Rechazar
+            </Button>
+          </Grid>
+          <Grid item xs={6}>
+            <Button variant="contained" color="primary" onClick={aceptarPerro}>
+              Aceptar
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={4}>
+        {/* Columna de perros aceptados */}
+        {perrosAceptados.map((perro) => (
+          <Card key={perro} sx={{ maxWidth: 400, borderRadius: 3 }}>
+            <CardMedia component="img" image={perro} alt="Perro" />
+          </Card>
+        ))}
+      </Grid>
+    </Grid>
+  );
+};
 
-export default App
+export default PaginaPerros;
